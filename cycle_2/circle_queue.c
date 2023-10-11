75517db16e561e16f1144ab52ea1431fc9ce2a94
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2023-10-11 16:27:34 +0530

    Add files via upload

---
+++ cycle_2/circle_queue.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+int i,front=0,rear=0,ch,a[20],n,x=0,item,temp;
+printf("\nEnter the limit for queue");
+scanf("%d",&n);
+do{
+printf("\nEnter the choice");
+printf("\n1. Insertion \t2. Deletion \t3. Display \t4.Exit");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:if(rear<=n)
+       {
+       printf("enter the element");
+       scanf("%d",&item);
+       a[rear]=item;
+       rear++;
+       break;
+       }
+       else if(x<front && front!=0)
+       {
+       printf("enter the elements");
+       scanf("%d",&item);
+       a[x]=item;
+       x=(x+1)%10;
+       break;
+       }
+       else
+       printf("Circular queue full");
+       break;
+case 2:if(front!=n)
+       front++;
+       else
+       printf("Queue is empty");
+       break;
+case 3:for(i=front;i<rear;i++)     
+       printf("%d\t",a[i]);
+       break;
+case 4:printf("\nExiting");
+       break;
+default:printf("\nWrong choice");
+}
+}while(ch!=4);
+return 0;
+}
+
